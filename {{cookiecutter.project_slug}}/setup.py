44f0b608a01ef8b384bb9deaa514c3cec71a0021
takahiro sakai <masterkuzutt@gmail.com>
2019-04-16 21:47:34 +0900

    add some cookiecutter pareamters

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='{{cookiecutter.project_slug}}',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='{{cookiecutter.project_short_description}}',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='{{cookiecutter.github_username}}',
+    author_email='',
+    url='https://github.com/masterkuzutt/personal_template',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
--- {{cookiecutter.project_slug}}/tests/test_advanced.py
+++ {{cookiecutter.project_slug}}/tests/test_advanced.py
@@ -9,7 +9,7 @@ class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_thoughts(self):
-        self.assertIsNone(sample.hmm())
+        self.assertIsNone( {{cookiecutter.project_slug}}.hmm())
 
 
 if __name__ == '__main__':
